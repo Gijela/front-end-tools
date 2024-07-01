a00e2c575efa65b74aa4f3252b5dc85769af1b23
linjianhui <linjianhui1@xdf.cn>
2024-07-01 13:55:58 +0800

    feat: add search and gpt

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,5 +1,9 @@
-export default function Home () {
+import IframeTemplate from '../iframeTemplate'
+
+export default function Home() {
   return (
-    <>Home</>
+    <div style={{ width: '100%', height: '100%' }}>
+      <IframeTemplate url="https://google.chat2hub.com" />
+    </div>
   )
 }
